001948f6f6706aa175bce0464d55680b319412f8
vijayme <vijay.sundhar@gmail.com>
2021-02-22 21:52:03 -0500

    DealTrackAPI

---
+++ DealTrackAPI/Controllers/DealTrackController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DealTrackAPI.Models;
+using DealTrackAPI.Request;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DealTrackAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DealTrackController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DealTrackController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<DealTrack>> GetAsync()
+
+        {
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return await _mediator.Send(new DealTrackRequest());
+        }
+
+        [HttpPost]
+        public async Task<HttpStatusCode> PostAsync([FromForm]object data)
+
+        {
+            return await _mediator.Send(new DealTrackPostRequest()
+            {
+                file = HttpContext.Request.Form.Files[0]
+            });
+        }
+    }
+}
---
+++ DealTrackAPI/DataAccess/DealTrackRepository.cs
@@ -0,0 +1,45 @@
+﻿using DealTrackAPI.Helpers.Decorators;
+using DealTrackAPI.Helpers.Extensions;
+using DealTrackAPI.Models;
+using Decor;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealTrackAPI.Data
+{
+    public class DealTrackRepository
+    {
+        [Decorate(typeof(PriceDecorator))]
+        public async Task<IEnumerable<DealTrack>> GetData()
+        {
+            try
+            {
+                return File.ReadAllLines("Dealertrack-CSV.csv")
+                    .Skip(1)
+                    .Select(v => new DealTrack().ReadFromCsv(v))
+                    .ToList();
+            }
+            catch
+            {
+                return new List<DealTrack>();
+            }
+        }
+
+        public void SaveData(IFormFile data)
+        {
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(data.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    result.AppendLine(reader.ReadLine());
+            }
+
+            System.IO.File.AppendAllText(@"Dealertrack-CSV.csv", result.ToString());
+        }
+    }
+}
