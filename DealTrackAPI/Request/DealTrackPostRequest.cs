54121651e4626740ef1ace516c483f66d69722d5
Vijay Mahadevan <vijay.sundhar@gmail.com>
2021-02-22 21:55:21 -0500

    Missed changes in first upload

---
+++ DealTrackAPI/Request/DealTrackPostRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DealTrackAPI.Request
+{
+    public class DealTrackPostRequest : IRequest<HttpStatusCode>
+    {
+        public IFormFile file {get;set;}
+    }
+
+}
---
+++ DealTrackAPI/Request/DealTrackRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DealTrackAPI.Models;
+
+namespace DealTrackAPI.Request
+{
+    public class DealTrackRequest : IRequest<IEnumerable<DealTrack>> { }
+}
